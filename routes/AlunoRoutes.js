d082d6ed7ab81482f00cdc8c96668f7318fc1a68
Pzin-BR <pedrosdrlfim@gmail.com>
2025-08-28 08:13:59 -0300

    first commit

---
+++ routes/AlunoRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+const router = express.Router()
+
+//busca o AlunoController
+
+import AlunoController from '../controllers/AlunoController.js'
+const controle = new AlunoController()
+
+const caminhobase = 'aluno/'
+
+router.get('/' + caminhobase + 'add', controle.openAdd)
+router.post('/' + caminhobase + 'add', controle.add)
+router.get('/' + caminhobase + 'lst', controle.list)
+
+export default router
